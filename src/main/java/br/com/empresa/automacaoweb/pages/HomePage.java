e12e0ef62ef58b9ef99852361317414bb17ee472
Willian Santana Costa <w7santana@gmail.com>
2020-09-07 11:04:51 -0300

    implementação de page object com lombok para gerar os getters e o construtor recebendo um driver e inicializandos os elementos

--- src/main/java/br/com/empresa/automacaoweb/pages/HomePage.java
+++ src/main/java/br/com/empresa/automacaoweb/pages/HomePage.java
@@ -1,5 +1,27 @@
 package br.com.empresa.automacaoweb.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import lombok.Getter;
+
+@Getter //Anotação do lombok que gera getters automaticamente
 public class HomePage {
+	
+	public HomePage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(id = "navbarDropdown")
+	private WebElement btnLogin;
+	
+	@FindBy(id = "exampleDropdownFormEmail2")
+	private WebElement campoEmail;
+	
+	@FindBy(id = "exampleDropdownFormPassword2")
+	private WebElement campoSenha;
+	
 
 }
